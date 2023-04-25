ba0b3027096007e9904416c1818b5342210c70b7
riahl12 <riley.ahlrichs@gmail.com>
2023-04-25 13:49:55 -0500

    new application repository & html page

---
+++ src/main/java/animal/repository/ApplicationRepository.java
@@ -0,0 +1,20 @@
+/**
+ *@author Riley Ahlrichs riahl12 - rpahlrichs
+ *CIS175 - Spring 2023
+ *Apr 25, 2023
+ */
+package animal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import animal.beans.AdopterApplicationForm;
+
+/**
+ * @author riley
+ *
+ */
+@Repository
+public interface ApplicationRepository extends JpaRepository<AdopterApplicationForm, Long>{
+
+}
